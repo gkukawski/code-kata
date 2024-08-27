2959209ec4310fae82e4ce8bc771e4d6b95ff437
Grzegorz Kukawski <gkukawski@gmail.com>
2024-08-27 11:59:31 +0200

    Reversed string function

---
+++ codewars/python/8kyu/reversed-strings.py
@@ -0,0 +1,12 @@
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# def solution(string):
+#     pass
+
+# https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+
+def solution(string):
+    return string[::-1]
