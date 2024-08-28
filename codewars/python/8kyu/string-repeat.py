34cf4be6176f86e42b229becd90141d89a654f52
Grzegorz Kukawski <gkukawski@gmail.com>
2024-08-28 09:21:55 +0200

    Repeat given string n times

---
+++ codewars/python/8kyu/string-repeat.py
@@ -0,0 +1,14 @@
+# Write a function that accepts an integer n and a string s
+# as parameters, and returns a string of s repeated exactly n times.
+
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+# def repeat_str(repeat, string):
+#     return ''
+
+# https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
+
+def repeat_str(n, s):
+    return n * s
