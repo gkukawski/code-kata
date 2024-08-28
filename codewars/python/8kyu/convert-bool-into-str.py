8b0678dc38b6b5d69e8c60ed2848fe327b19e672
Grzegorz Kukawski <gkukawski@gmail.com>
2024-08-28 09:05:44 +0200

    Converting booleean to string

---
+++ codewars/python/8kyu/convert-bool-into-str.py
@@ -0,0 +1,10 @@
+# Complete the method that takes a boolean value and return a
+# "Yes" string for true, or a "No" string for false.
+
+# def bool_to_word(boolean):
+#     # TODO
+
+# https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
