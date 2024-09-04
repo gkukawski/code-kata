64fe8facf15d0132fad68d6a4cd8da3562c9c8fe
Grzegorz Kukawski <gkukawski@gmail.com>
2024-09-04 10:49:18 +0200

    Convert RGB to hex representation

---
+++ codewars/python/5kyu/rgb-to-hex-conversion.py
@@ -0,0 +1,19 @@
+# Description:
+# The rgb function is incomplete. Complete it so that passing in RGB
+# decimal values will result in a hexadecimal representation being returned.
+# Valid decimal values for RGB are 0 - 255. Any values that fall out of
+# that range must be rounded to the closest valid value.
+
+# Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+# Examples (input --> output):
+# 255, 255, 255 --> "FFFFFF"
+# 255, 255, 300 --> "FFFFFF"
+# 0, 0, 0       --> "000000"
+# 148, 0, 211   --> "9400D3"
+
+
+# https://www.codewars.com/kata/513e08acc600c94f01000001/python
+
+def rgb(r, g, b):
+    return (f'{max(min(r,255),0):x}'.zfill(2) + f'{max(min(g,255),0):x}'.zfill(2) + f'{max(min(b,255),0):x}'.zfill(2)).upper()
